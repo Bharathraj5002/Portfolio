f9a460a17fa9d61d195dcace6c32813899b608de
bharthraj5002 <bharathraje2005@gmail.com>
2025-09-01 10:32:23 +0530

    final testing

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,3 +1,4 @@
+
 # Generate an SSH key pair in Terraform (kept in state)
 resource "tls_private_key" "portfolio" {
   algorithm = "RSA"
@@ -11,8 +12,8 @@ resource "aws_key_pair" "portfolio" {
 
 # Save the private key to a .pem file locally
 resource "local_file" "portfolio_private_key" {
-  content         = tls_private_key.portfolio.private_key_pem
-  filename        = "${path.module}/portfolio.pem"
+  content  = tls_private_key.portfolio.private_key_pem
+  filename = "${path.module}/portfolio.pem"
   file_permission = "0600"
 }
 
@@ -71,7 +72,7 @@ data "aws_subnets" "default_vpc_subnets" {
   }
 }
 
-# Ubuntu AMI (latest for us-east-2, Ubuntu 20.04 focal)
+# Ubuntu AMI (latest for us-east-2)
 data "aws_ami" "ubuntu" {
   most_recent = true
   owners      = ["099720109477"] # Canonical
@@ -89,7 +90,6 @@ resource "aws_instance" "portfolio" {
   subnet_id              = element(data.aws_subnets.default_vpc_subnets.ids, 0)
   vpc_security_group_ids = [aws_security_group.portfolio_sg.id]
   key_name               = aws_key_pair.portfolio.key_name
-
   # Install dependencies (Docker)
   user_data = <<-EOF
               #!/bin/bash
@@ -108,7 +108,6 @@ resource "aws_instance" "portfolio" {
               # Allow ubuntu user to run Docker without sudo
               usermod -aG docker ubuntu
               EOF
-
   tags = {
     Name = "portfolio"
   }
